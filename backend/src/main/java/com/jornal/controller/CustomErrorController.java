81cc0dd992676cf367b0f0f5a24c1862f4841fa1
globalfocus <franciscocauanoliveiro@gmail.com>
2025-09-05 15:46:52 -0300

    Descreva aqui as alterações feitas

---
+++ backend/src/main/java/com/jornal/controller/CustomErrorController.java
@@ -0,0 +1,25 @@
+package com.jornal.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public ResponseEntity<Map<String, Object>> handleError(HttpServletRequest request) {
+        Map<String, Object> errorDetails = new HashMap<>();
+        errorDetails.put("message", "An error occurred");
+        errorDetails.put("path", request.getRequestURI());
+        errorDetails.put("status", HttpStatus.NOT_FOUND.value());
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+}
---
+++ backend/src/main/java/com/jornal/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.jornal.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("Jornal da EEEP Adolfo Ferreira de Sousa API");
+    }
+}
